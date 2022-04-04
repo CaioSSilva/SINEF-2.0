2713547215689d2825d94e10364bd8ef467d2ce1
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-04-04 08:50:06 -0300

    data script add

---
+++ Area do Aluno/Javascript/script.js
@@ -0,0 +1,9 @@
+var data = new Date();
+var dia = String(data.getDate()).padStart(2, '0');
+var mes = String(data.getMonth() + 1).padStart(2, '0');
+var ano = data.getFullYear();
+dataAtual = dia + '/' + mes + '/' + ano;
+
+document.querySelector(".data").innerHTML = dataAtual;
+
+console.log(dataAtual);
