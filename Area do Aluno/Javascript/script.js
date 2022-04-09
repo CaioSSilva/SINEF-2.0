879dba167e203ca886d50f374f6fe4582b6a9220
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-04-09 11:31:36 -0300

    menu drop add

--- Area do Aluno/Javascript/script.js
+++ Area do Aluno/Javascript/script.js
@@ -5,3 +5,42 @@ var ano = data.getFullYear();
 dataAtual = dia + '/' + mes + '/' + ano;
 
 document.querySelector(".data").innerHTML = dataAtual;
+
+
+let acadOpt = document.querySelector('.academico-options');
+let didOpt = document.querySelector('.didatico-options');
+let eventOpt = document.querySelector('.eventos-options');
+let finanOpt = document.querySelector('.financeiro-options');
+
+function menuAcad(){
+    if(acadOpt.style.display == "none"){
+        acadOpt.style.display = "block"
+    }
+    else{
+        acadOpt.style.display = "none" 
+    }
+}
+function menuDid(){
+    if(didOpt.style.display == "none"){
+        didOpt.style.display = "block"
+    }
+    else{
+        didOpt.style.display = "none" 
+    }
+}
+function menuEvent(){
+    if(eventOpt.style.display == "none"){
+        eventOpt.style.display = "block"
+    }
+    else{
+        eventOpt.style.display = "none" 
+    }
+}
+function menuFinan(){
+    if(finanOpt.style.display == "none"){
+        finanOpt.style.display = "block"
+    }
+    else{
+        finanOpt.style.display = "none" 
+    }
+}
