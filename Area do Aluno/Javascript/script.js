fe05facf17c1d81ce4c9b152c334dc286b68c866
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-05-15 10:41:00 -0300

    Options menu add

--- Area do Aluno/Javascript/script.js
+++ Area do Aluno/Javascript/script.js
@@ -1,3 +1,4 @@
+//Data Atual
 var data = new Date();
 var dia = String(data.getDate()).padStart(2, '0');
 var mes = String(data.getMonth() + 1).padStart(2, '0');
@@ -6,41 +7,71 @@ dataAtual = dia + '/' + mes + '/' + ano;
 
 document.querySelector(".data").innerHTML = dataAtual;
 
-
-let acadOpt = document.querySelector('.academico-options');
-let didOpt = document.querySelector('.didatico-options');
-let eventOpt = document.querySelector('.eventos-options');
-let finanOpt = document.querySelector('.financeiro-options');
-
-function menuAcad(){
-    if(acadOpt.style.display == "none"){
-        acadOpt.style.display = "block"
-    }
-    else{
-        acadOpt.style.display = "none" 
-    }
-}
-function menuDid(){
-    if(didOpt.style.display == "none"){
-        didOpt.style.display = "block"
-    }
-    else{
-        didOpt.style.display = "none" 
-    }
-}
-function menuEvent(){
-    if(eventOpt.style.display == "none"){
-        eventOpt.style.display = "block"
-    }
-    else{
-        eventOpt.style.display = "none" 
-    }
-}
-function menuFinan(){
-    if(finanOpt.style.display == "none"){
-        finanOpt.style.display = "block"
-    }
-    else{
-        finanOpt.style.display = "none" 
-    }
-}
+//Menu Options
+const academicoOptions = document.getElementById("academico-options")
+const didaticoOptions = document.getElementById("didatico-options")
+const eventosOptions = document.getElementById("eventos-options")
+const financeiroOptions = document.getElementById("financeiro-options")
+//Botoes Menu
+const academicoButton = document.getElementById("academico")
+const didaticoButton = document.getElementById("didatico")
+const eventosButton = document.getElementById("eventos")
+const financeiroButton = document.getElementById("financeiro")
+//Academico Hover
+academicoButton.addEventListener('mouseover', () =>{
+    academicoOptions.style.display = 'block'
+})
+academicoButton.addEventListener('mouseout', () =>{
+    academicoOptions.style.display = 'none'
+})
+academicoOptions.addEventListener('mouseover', () =>{
+    academicoOptions.style.display = 'block'
+})
+academicoOptions.addEventListener('mouseout', () =>{
+    academicoOptions.style.display = 'none'
+})
+//Didatico Hover
+didaticoButton.addEventListener('mouseover', () =>{
+    didaticoOptions.style.display = 'block'
+    let OptionsHeight = (document.getElementById("academico").clientHeight + 25)
+    didaticoOptions.style.marginTop = (OptionsHeight + 'px')
+})
+didaticoButton.addEventListener('mouseout', () =>{
+    didaticoOptions.style.display = 'none'
+})
+didaticoOptions.addEventListener('mouseover', () =>{
+    didaticoOptions.style.display = 'block'
+})
+didaticoOptions.addEventListener('mouseout', () =>{
+    didaticoOptions.style.display = 'none'
+})
+//Eventos Hover
+eventosButton.addEventListener('mouseover', () =>{
+    eventosOptions.style.display = 'block'
+    let OptionsHeight = (document.getElementById("academico").clientHeight + 25)
+    eventosOptions.style.marginTop = (OptionsHeight * 2) + 'px'
+})
+eventosButton.addEventListener('mouseout', () =>{
+    eventosOptions.style.display = 'none'
+})
+eventosOptions.addEventListener('mouseover', () =>{
+    eventosOptions.style.display = 'block'
+})
+eventosOptions.addEventListener('mouseout', () =>{
+    eventosOptions.style.display = 'none'
+})
+//Finaceiro Hover
+financeiroButton.addEventListener('mouseover', () =>{
+    financeiroOptions.style.display = 'block'
+    let OptionsHeight = (document.getElementById("academico").clientHeight + 25)
+    financeiroOptions.style.marginTop = (OptionsHeight * 3) + 'px'
+})
+financeiroButton.addEventListener('mouseout', () =>{
+    financeiroOptions.style.display = 'none'
+})
+financeiroOptions.addEventListener('mouseover', () =>{
+    financeiroOptions.style.display = 'block'
+})
+financeiroOptions.addEventListener('mouseout', () =>{
+    financeiroOptions.style.display = 'none'
+})
