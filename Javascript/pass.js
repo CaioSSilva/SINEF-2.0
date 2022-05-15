9681bedce2573190517046ed5da72f6e37208f0c
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-05-15 11:33:52 -0300

    Login add

--- Javascript/pass.js
+++ Javascript/pass.js
@@ -1,12 +1,23 @@
-function verificaLogin(){
-    const email = document.querySelector('.email').value;
-    const senha = document.querySelector('.senha').value;
-    if(email == "aluno" && senha == "aluno"){
+const email = document.querySelector('.email');
+const senha = document.querySelector('.senha');
+const loginButton = document.getElementById('loginButton')
+const usuarios = [
+    {
+        email: "marcosalmeida@fumec.br",
+        senha: "marcos1234"
+    },
+]
+
+if(loginButton){
+    loginButton.addEventListener('click', () =>{
+        if(email.value === usuarios[0].email && senha.value === usuarios[0].senha){
             location.href = "Area do Aluno/index.html"
         }else{
-                alert("Erro! Dados Incorretos")
-            }
+            alert("Dados Incorretos!")
+            location.reload()
         }
-function esqueciDados(){
-    alert("(Email: aluno) (Senha: aluno)  Por Favor, guarde seus dados!")
+    })
+}
+function forgotData(){
+    alert(" Email: marcosalmeida@fumec.br    Senha: marcos1234")
 }
